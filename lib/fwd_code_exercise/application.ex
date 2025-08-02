a5d8cddc0e2f034215b7b19536e104bbb50c7dfc
Jordan <jordandmoossazadeh@gmail.com>
2025-08-02 12:51:03 -0700

    update Application doc and specs again

--- lib/fwd_code_exercise/application.ex
+++ lib/fwd_code_exercise/application.ex
@@ -1,16 +1,16 @@
 defmodule FwdCodeExercise.Application do
   @moduledoc """
-  The main application module for application.
+  The main application module for the project.
   Starts the application and its supervision tree.
   """
 
   use Application
 
-  @impl true
-  @spec start(any(), any()) :: {:ok, pid()} | {:error, {:already_started, pid()} | {:shutdown, term()} | term()}
+  @spec start(Application.start_type(), start_args :: term())
+  :: {:ok, pid()} | {:error, {:already_started, pid()} | {:shutdown, term()} | term()}
   def start(_type, _args) do
     children = [
-      {Bandit, plug: FwdCodeExercise.Router, scheme: :http, port: 4000}
+      {Bandit, plug: FwdCodeExercise.Router}
     ]
 
     opts = [strategy: :one_for_one, name: FwdCodeExercise.Supervisor]

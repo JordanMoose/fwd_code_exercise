f43adb11542d27030703e7d6613dc73a3529dc92
Jordan <jordandmoossazadeh@gmail.com>
2025-08-04 00:40:05 -0700

    don't start poller in test env

--- lib/fwd_code_exercise/application.ex
+++ lib/fwd_code_exercise/application.ex
@@ -17,11 +17,18 @@ defmodule FwdCodeExercise.Application do
     children = [
       {Phoenix.PubSub, name: FwdCodeExercise.PubSub},
       {Bandit, plug: FwdCodeExercise.Router, ip: :loopback, port: 4000},
-      FwdCodeExercise.ArcGisPoller,
       {FwdCodeExercise.SocketClient, Application.get_env(:fwd_code_exercise, :websocket_url, "ws://localhost:4000/")}
-    ]
+    ] ++ poller_client()
 
     opts = [strategy: :one_for_one, name: FwdCodeExercise.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp poller_client() do
+    if Mix.env() == :test do
+      []
+    else
+      [FwdCodeExercise.ArcGisPoller]
+    end
   end
 end
